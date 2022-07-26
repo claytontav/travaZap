aebd98ba0d69ec489b0dbb19744e17d6a230861a
Clayton Tavares <54451974+claytongit@users.noreply.github.com>
2022-07-26 10:25:30 -0300

    Update travaZap.js

--- travaZap.js
+++ travaZap.js
@@ -13,7 +13,7 @@ function enviarScript(scriptText){
     
             const event = new InputEvent('input', { bubbles: true });
     
-            const textbox = document.querySelector('#main div._3HQNh');
+            const textbox = document.querySelector('#main div._2lMWa');
     
             textbox.textContent = lines[i];
     
